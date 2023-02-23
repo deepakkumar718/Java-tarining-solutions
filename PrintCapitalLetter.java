46fd331a7685bdf526ba11ac91b94875faea8286
deepakkumar718 <126136567+deepakkumar718@users.noreply.github.com>
2023-02-23 16:29:42 +0530

    Add files via upload

---
+++ PrintCapitalLetter.java
@@ -0,0 +1,8 @@
+package selfpractice;
+
+public class PrintCapitalLetter {
+    public static void main(String[]args){
+        String txt = "Hello World";
+        System.out.println(txt.toUpperCase());
+    }
+}
---
+++ UpperCase.java
@@ -0,0 +1,10 @@
+package selfpractice;
+
+public class UpperCase {
+    public static void main(String[] args) {
+        String str = "b.v.raju college ";
+        String strUpper = str.toUpperCase();
+        System.out.println("Original String: " + str);
+        System.out.println("String changed to upper case: " + strUpper);
+    }
+}
